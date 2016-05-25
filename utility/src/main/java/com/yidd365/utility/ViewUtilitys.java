e2d7436b9ff010d8f3360c99f07d95d81375caeb
陈志 <orinchen.d@gamil.com>
2016-05-25 10:26:11 +0800

    给 ViewUtility 添加dp2px 和 px2dp

--- utility/src/main/java/com/yidd365/utility/ViewUtilitys.java
+++ utility/src/main/java/com/yidd365/utility/ViewUtilitys.java
@@ -4,13 +4,16 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
 
+
 /**
  * Created by orinchen on 16/5/19.
  */
-public final class ViewUtils {
+public final class ViewUtilitys {
     private static Integer width = null;
     private static Integer height = null;
-    private ViewUtils(){}
+    private static Float scale = null;
+
+    private ViewUtilitys(){}
 
     public static int getScreenWidth(Context context) {
         if(width == null) {
@@ -30,5 +33,26 @@ public final class ViewUtils {
             height = metrics.heightPixels;
         }
         return height;
+    }
+
+    public static float getScreenScale(Context context){
+        if(scale == null){
+            scale = context.getResources().getDisplayMetrics().density;
+        }
+        return scale;
+    }
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        return (int) (dpValue * getScreenScale(context) + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        return (int) (pxValue / getScreenScale(context) + 0.5f);
     }
 }
